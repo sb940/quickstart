bb107239c4412b6103e3b09444824c279b694599
sb940 <31840465+sb940@users.noreply.github.com>
2017-09-18 17:17:42 +0800

    3
    3

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -5,6 +5,7 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'my-dashboard',
   templateUrl: './dashboard.component.html',
+  styleUrls: [ './dashboard.component.css' ]
 })
 export class DashboardComponent implements OnInit {
 
@@ -14,6 +15,6 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit(): void {
     this.heroService.getHeroes()
-      .then(heroes => this.heroes = heroes.slice(1, 5));
+      .then(heroes => this.heroes = heroes.slice(0, 5));
   }
 }
